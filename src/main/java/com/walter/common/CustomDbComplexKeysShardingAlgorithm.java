905d504c285377c3f087178c331722193d183549
tanyunxing <tanyunxing@yy.com>
2021-02-10 13:38:33 +0800

    自定义分片策略，修复IN语句时分片错误的问题

--- src/main/java/com/walter/common/CustomDbComplexKeysShardingAlgorithm.java
+++ src/main/java/com/walter/common/CustomDbComplexKeysShardingAlgorithm.java
@@ -1,13 +1,14 @@
 package com.walter.common;
 
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingValue;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
@@ -30,17 +31,21 @@ public class CustomDbComplexKeysShardingAlgorithm implements ComplexKeysSharding
     public Collection<String> doSharding(Collection<String> availableTargetNames, ComplexKeysShardingValue<Long> complexKeysShardingValue) {
         log.info("[CustomDbComplexKeysShardingAlgorithm.doSharding] start: {}, {}", availableTargetNames, complexKeysShardingValue);
 
-        Long shardingValue = complexKeysShardingValue.getColumnNameAndShardingValuesMap().values().stream()
-                .flatMap(Collection::stream).findFirst().orElseThrow(() -> new RuntimeException("No sharding value found"));
+        List<String> resultList = Lists.newArrayList();
 
-        long dbIndex = shardingValue % availableTargetNames.size();
-        String dbName = new StringBuilder(shardPrefix == null ? DEFAULT_SHARD_PREFIX : shardPrefix).append(dbIndex).toString();
+        complexKeysShardingValue.getColumnNameAndShardingValuesMap().values()
+                .stream().flatMap(Collection::stream).forEach(shardingValue -> {
+            long dbIndex = shardingValue % availableTargetNames.size();
+            String dbName = new StringBuilder(shardPrefix == null ? DEFAULT_SHARD_PREFIX : shardPrefix).append(dbIndex).toString();
 
-        if(Objects.nonNull(availableTargetNames)
-                && availableTargetNames.stream().map(String::toLowerCase).anyMatch(Predicate.isEqual(dbName.toLowerCase()))){
-            return Arrays.asList(dbName);
-        }
+            if(Objects.nonNull(availableTargetNames)
+                    && availableTargetNames.stream().map(String::toLowerCase).anyMatch(Predicate.isEqual(dbName.toLowerCase()))){
+                resultList.add(dbName);
+            }else{
+                throw new RuntimeException("No sharding result found for sharding value: " + shardingValue);
+            }
+        });
 
-        return Arrays.asList();
+        return resultList;
     }
 }
--- src/main/java/com/walter/common/CustomTableComplexKeysShardingAlgorithm.java
+++ src/main/java/com/walter/common/CustomTableComplexKeysShardingAlgorithm.java
@@ -1,11 +1,12 @@
 package com.walter.common;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingAlgorithm;
 import org.apache.shardingsphere.api.sharding.complex.ComplexKeysShardingValue;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
@@ -22,17 +23,20 @@ public class CustomTableComplexKeysShardingAlgorithm implements ComplexKeysShard
     public Collection<String> doSharding(Collection<String> availableTargetNames, ComplexKeysShardingValue<Long> complexKeysShardingValue) {
         log.info("[CustomTableComplexKeysShardingAlgorithm.doSharding] start: {}, {}", availableTargetNames, complexKeysShardingValue);
 
-        Long shardingValue = complexKeysShardingValue.getColumnNameAndShardingValuesMap().values().stream()
-                .flatMap(Collection::stream).findFirst().orElseThrow(() -> new RuntimeException("No sharding value found"));
+        List<String> resultList = Lists.newArrayList();
 
-        long tableIndex = shardingValue % availableTargetNames.size();
-        String tableName = new StringBuilder(complexKeysShardingValue.getLogicTableName()).append(tableIndex).toString();
+        complexKeysShardingValue.getColumnNameAndShardingValuesMap().values()
+                .stream().flatMap(Collection::stream).forEach(shardingValue -> {
+            long tableIndex = shardingValue % availableTargetNames.size();
+            String tableName = new StringBuilder(complexKeysShardingValue.getLogicTableName()).append(tableIndex).toString();
+            if(Objects.nonNull(availableTargetNames)
+                    && availableTargetNames.stream().map(String::toLowerCase).anyMatch(Predicate.isEqual(tableName.toLowerCase()))){
+                resultList.add(tableName);
+            }else{
+                throw new RuntimeException("No sharding result found for sharding value: " + shardingValue);
+            }
+        });
 
-        if(Objects.nonNull(availableTargetNames)
-                && availableTargetNames.stream().map(String::toLowerCase).anyMatch(Predicate.isEqual(tableName.toLowerCase()))){
-            return Arrays.asList(tableName);
-        }
-
-        return Arrays.asList();
+        return resultList;
     }
 }

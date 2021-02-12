93d390b967911ef792712791e14a9d2ac080f462
tanyunxing <tanyunxing@yy.com>
2021-02-12 14:57:42 +0800

    使用自定义SQLExecutionHook统计一次执行的耗时; JDBC方式下通过ExecutionUnit获取一次执行中的分片、改写后的SQL及入参等信息

--- src/test/java/com/walter/JdbcTests.java
+++ src/test/java/com/walter/JdbcTests.java
@@ -1,9 +1,13 @@
 package com.walter;
 
+import com.walter.util.JsonUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shardingsphere.shardingjdbc.jdbc.core.resultset.ShardingResultSet;
+import org.apache.shardingsphere.underlying.executor.context.ExecutionUnit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,12 +16,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
 
 @Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JdbcTests {
+
     @Autowired
+    @Qualifier("shardingDataSource")
     private DataSource dataSource;
 
     @Test
@@ -29,6 +36,7 @@ public class JdbcTests {
             preparedStatement.setLong(1, 9785L);
             preparedStatement.setLong(2, 4740584156199727105L);
             try (ResultSet rs = preparedStatement.executeQuery()) {
+                log.info("[ExecutionUnits]: {}", JsonUtil.toJson(getExecutionUnitSet(rs)));
                 while(rs.next()) {
                     log.info("result: orderId={}, userId={}, status={}, orderItemId={}", rs.getLong("order_id"), rs.getLong("user_id"),
                             rs.getString("status"), rs.getLong("order_item_id"));
@@ -45,11 +53,19 @@ public class JdbcTests {
              PreparedStatement preparedStatement = conn.prepareStatement(SQL)) {
             preparedStatement.setLong(1, 12558L);
             try (ResultSet rs = preparedStatement.executeQuery()) {
+                log.info("[ExecutionUnits]: {}", JsonUtil.toJson(getExecutionUnitSet(rs)));
                 while(rs.next()) {
                     log.info("result: orderId={}, userId={}, status={}, orderItemId={}", rs.getLong("order_id"), rs.getLong("user_id"),
                             rs.getString("status"), rs.getLong("order_item_id"));
                 }
             }
         }
+    }
+
+    private Collection<ExecutionUnit> getExecutionUnitSet(ResultSet rs){
+        if(rs instanceof ShardingResultSet){
+            return ((ShardingResultSet)rs).getExecutionContext().getExecutionUnits();
+        }
+        return null;
     }
 }
